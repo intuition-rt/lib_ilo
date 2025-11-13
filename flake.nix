211340a4712ec46d32e9ec266d74b6b861e01d87
Sigmanificient <edhyjox@gmail.com>
2025-11-13 15:38:59 +0100

    setup flake with deps

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        hardeningDisable = ["fortify"];
+
+        packages = with pkgs; [
+          clang
+          platformio
+          clang-tools
+          scom
+        ];
+      };
+    });
+
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+  };
+}
