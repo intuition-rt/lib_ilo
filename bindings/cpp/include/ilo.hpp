13d796ef7faee1a030fcfc2c149f750d0aed5112
Savinien Petitjean <savinien.petitjean@gmail.com>
2025-11-21 18:23:03 +0100

    Add cpp bindings

---
+++ bindings/cpp/include/ilo.hpp
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+
+class Ilo {
+  public:
+    void setWifiCredentials(String ssid, String password);
+};
---
+++ bindings/cpp/src/ilo.cpp
@@ -0,0 +1,21 @@
+#include "../include/ilo.hpp"
+
+#include "core/trame.h"
+
+#undef build_trame
+#define build_trame(name, ...) \
+  ({ \
+    trame_param tmp_params[] = __VA_ARGS__; \
+    exposed_build_trame(name, countof(tmp_params), tmp_params); \
+  })
+
+void Ilo::setWifiCredentials(String ssid, String password) {
+  char *p = build_trame(
+        "set_wifi_credentials", {
+            { "ssid", ssid.c_str() },
+            { "password", password.c_str() },
+        }
+   );
+
+  printf("[%s]\n", p);
+}
