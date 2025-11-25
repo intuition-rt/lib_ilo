915aa520bd7e2014b417be7e2c42ad96449a40cb
Savinien Petitjean <savinien.petitjean@gmail.com>
2025-11-25 15:01:06 +0100

    Add serial com in cpp binding

--- bindings/cpp/include/ilo.hpp
+++ bindings/cpp/include/ilo.hpp
@@ -1,6 +1,17 @@
+#pragma once
+
 #include <Arduino.h>
+
+#define SERIAL_RX_PIN 17
+#define SERIAL_TX_PIN 16
+#define SERIAL_BAUD 115200
+#define SERIAL_CONFIG SERIAL_8N1
 
 class Ilo {
   public:
+    Ilo();
+
+    void handshakeIlo();
     void setWifiCredentials(String ssid, String password);
+    void getWifiCredentials();
 };
--- bindings/cpp/src/ilo.cpp
+++ bindings/cpp/src/ilo.cpp
@@ -1,8 +1,9 @@
+#include <Arduino.h>
+
 #include "../include/ilo.hpp"
 
-
 extern "C" {
-#include "core/trame.h"
+  #include "core/trame.h"
 }
 
 #undef build_trame
@@ -12,13 +13,35 @@ extern "C" {
     exposed_build_trame(name, countof(tmp_params), tmp_params); \
   })
 
+
+Ilo::Ilo() {
+  Serial2.begin(SERIAL_BAUD, SERIAL_CONFIG, SERIAL_RX_PIN, SERIAL_TX_PIN);
+}
+
+void Ilo::handshakeIlo() {
+  char *p = build_trame(
+      "handshake_ilo", {}
+  );
+
+  Serial2.println(String(p));
+}
+
+void Ilo::getWifiCredentials() {
+  char *p = build_trame(
+      "get_wifi_credentials", {}
+  );
+
+  Serial2.println(String(p));
+}
+
 void Ilo::setWifiCredentials(String ssid, String password) {
   char *p = build_trame(
-        "set_wifi_credentials", {
-            { "ssid", ssid.c_str() },
-            { "password", password.c_str() },
-        }
-   );
+      "set_wifi_credentials", {
+        { "ssid", ssid.c_str() },
+        { "password", password.c_str() },
+      }
+  );
 
   printf("[%s]\n", p);
+  Serial2.print(String(p));
 }
