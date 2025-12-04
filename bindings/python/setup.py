7906b5f95a14f6700d8cdf861832570b09f4a2d1
Sigmanificient <edhyjox@gmail.com>
2025-12-04 17:20:56 +0100

    Remove leftover python extension

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,32 +1,5 @@
-import os
-from typing import Iterator
+from setuptools import setup
 
-from setuptools import Extension, setup
-
-
-def find_sources(basepath: str) -> Iterator[str]:
-    files = (
-        (current_dir, file)
-        for current_dir, _, files in os.walk(basepath, followlinks=True)
-        for file in files
-    )
-
-    for current_dir, file in files:
-        *_, ext = os.path.splitext(file)
-        if ext != f"{os.path.extsep}c":
-            continue
-
-        fullpath = os.path.join(current_dir, file)
-        print(f"Registering ${fullpath} to sources")
-        yield fullpath
-
-
-ilo_binding_ext = Extension(
-    "ilo_binding_ext",
-    sources=list(find_sources("ilo_binding_ext")),
-    include_dirs=["ilo_binding_ext"],
-    language="c",
-)
 
 if __name__ == "__main__":
-    setup(ext_modules=[ilo_binding_ext])
+    setup()
